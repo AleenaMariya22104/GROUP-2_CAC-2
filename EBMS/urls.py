048011fca1b289f89d13e0f252137a0259e99ae4
Tanuja Gupta <tanuja.gupta@msds.christuniversity.in>
2024-01-20 18:24:40 +0530

    Tanuja

--- EBMS/urls.py
+++ EBMS/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from BILLING.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # billing
+
+    path('',home,name='home'),
 ]
